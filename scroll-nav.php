119343c68b89edaa7a319446937c013d0b8765cd
bestrag <aleksandar.bestrag@gmail.com>
2013-09-30 15:56:21 +0200

    DMS Only Scroll Nav && Scroll Nav Fixed Initial commit

---
+++ scroll-nav.php
@@ -0,0 +1,10 @@
+<?php
+/*
+	Plugin Name: Scroll Nav
+	Author: .bestrag
+	Author URI: http://www.bestrag.net
+	Description: Ultimate one-page scroll navigation.
+	Demo:
+	Version: 3.1
+	Filter: nav
+*/
